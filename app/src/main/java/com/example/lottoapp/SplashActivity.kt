b59924d700b5762e215fa8737ac1ee81dd310cf6
kimjw2003 <kimjw111003@gmail.com>
2020-09-17 18:10:32 +0900

    ADD setOnClickListener

--- app/src/main/java/com/example/lottoapp/SplashActivity.kt
+++ app/src/main/java/com/example/lottoapp/SplashActivity.kt
@@ -1,11 +1,19 @@
 package com.example.lottoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        animationView.setOnClickListener{
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
